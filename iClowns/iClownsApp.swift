86e5425c4947becf29eb8176294ed3a44d028932
giuseppeolivari <golivari1@gmail.com>
2024-05-13 12:42:46 +0200

    test1

---
+++ iClowns/iClownsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iClownsApp.swift
+//  iClowns
+//
+//  Created by Giuseppe Olivari on 13/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iClownsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
