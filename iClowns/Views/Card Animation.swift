7d710b87a16a09eeec71e8192b08aa17750225a6
giuseppeolivari <golivari1@gmail.com>
2024-06-11 10:14:50 +0200

    animazione

--- iClowns/Views/Card Animation.swift
+++ iClowns/Views/Card Animation.swift
@@ -23,16 +23,11 @@ struct CardFront : View {
                 .fill(.white)
                 .frame(width: width, height: height)
                 .shadow(color: .gray, radius: 2, x: 0, y: 0)
-            /*
-            Image(systemName: "suit.club.fill")
-                .resizable()
-                .frame(width: 80, height: 80)
-                .foregroundColor(.red)
-             */
             
-            Text("FRONTE")
-                .bold()
-                .font(.largeTitle)
+          //  Image(systemName: "suit.club.fill")
+             
+            
+           
 
         }.rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
     }
@@ -49,34 +44,8 @@ struct CardBack : View {
 
     var body: some View {
         ZStack {
-            
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(.blue.opacity(0.7), lineWidth: 3)
-                .frame(width: width, height: height)
-
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.blue.opacity(0.2))
-                .frame(width: width, height: height)
-                .shadow(color: .gray, radius: 2, x: 0, y: 0)
-
-            RoundedRectangle(cornerRadius: 20)
-                .fill(.blue.opacity(0.7))
-                .padding()
-                .frame(width: width, height: height)
-
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(.blue.opacity(0.7), lineWidth: 3)
-                .padding()
-                .frame(width: width, height: height)
-
-            Text("RETRO")
-                .bold()
-                .font(.largeTitle)
-            
-            Image(systemName: "seal")
-                .resizable()
-                .frame(width: 150, height: 150)
-                .foregroundColor(.blue.opacity(0.7))
+               
+            Image("retrofrankobolls")
 
         }.rotation3DEffect(Angle(degrees: degree), axis: (x: 0, y: 1, z: 0))
 
